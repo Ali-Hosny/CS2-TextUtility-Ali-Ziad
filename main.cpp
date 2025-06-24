ea6beddd6380ec1e670d5b0079541049f0cc7b8f
Ziad Eliwa <ziadeliwa@aucegypt.edu>
2025-06-24 21:19:37 +0300

    Added Pallindrome Checking in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include "textutility.h"
+
+const int N = 100000;
 int main()
 {
+    char pallindrome[N];
+    int size = 0;
+    std::cout << "Enter the size of string: \n";
+    std::cin >> size;
+    std::cin.getline(pallindrome,N);
+    std::cout << "You entered the Cstring: " << pallindrome << '\n';
+    if(pallindromeCheck(pallindrome,size))
+    {
+        std::cout << "This is a pallindrome.\n";
+    }
+   else
+    {
+        std::cout << "This is not a pallindrome.\n";
+    }
+    
     return 0;
 }
