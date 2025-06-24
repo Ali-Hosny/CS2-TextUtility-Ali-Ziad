3bbf35df8bfdf031b3b8d4307c3b60552dc81188
Ziad Eliwa <ziadeliwa@aucegypt.edu>
2025-06-24 21:21:04 +0300

    Bug Fixed in toLower and toUpper Functions

--- textutility.cpp
+++ textutility.cpp
@@ -16,7 +16,7 @@ void toUpper(char arr[], int size)
 {
     for (int i = 0; i < size; i++)
     {
-        if(arr[i] >= 97)
+        if(arr[i] >= 'a' && arr[i] <= 'z')
         {
             arr[i] -= 32;
         }
@@ -26,7 +26,7 @@ void toLower(char arr[], int size)
 {
     for (int i = 0; i < size; i++)
     {
-        if(arr[i] < 97)
+        if(arr[i] <= 'Z' && arr[i] >= 'A')
         {
             arr[i] += 32;
         }
