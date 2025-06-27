13b5602538093b3d41d9590f09d4c0b13216fb7d
Ali Hosny <alihosny7ul@gmail.com>
2025-06-27 17:28:43 +0300

    Made Cmake

---
+++ build/CMakeFiles/trial.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/aliho/CS2-TextUtility-Ali-Ziad/main.cpp" "CMakeFiles/trial.dir/main.cpp.obj" "gcc" "CMakeFiles/trial.dir/main.cpp.obj.d"
+  "C:/Users/aliho/CS2-TextUtility-Ali-Ziad/textutility.cpp" "CMakeFiles/trial.dir/textutility.cpp.obj" "gcc" "CMakeFiles/trial.dir/textutility.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/trial.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trial.dir/main.cpp.obj"
+  "CMakeFiles/trial.dir/main.cpp.obj.d"
+  "CMakeFiles/trial.dir/textutility.cpp.obj"
+  "CMakeFiles/trial.dir/textutility.cpp.obj.d"
+  "libtrial.dll.a"
+  "trial.exe"
+  "trial.exe.manifest"
+  "trial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
